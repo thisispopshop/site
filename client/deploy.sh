3a8efc95b11d03edb8fa35e1bdcdfa373d4c5039
kenakingkong <kenak@DESKTOP-T22M1KF.localdomain>
2019-03-08 21:49:26 -0800

    changed repo name

--- client/deploy.sh
+++ client/deploy.sh
@@ -16,7 +16,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:kenakingkong/kenakingkong.github.io.git master
+#git push -f git@github.com:kenakingkong/kenakingkong.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
