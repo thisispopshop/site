b70538be4504ca08245a8866af2db8e88a932b6e
kenakingkong <kenak@DESKTOP-T22M1KF.localdomain>
2019-03-22 21:32:39 -0700

    testing node on nginx

--- api/bin/app.ts
+++ api/bin/app.ts
@@ -2,6 +2,9 @@ import express from "express";
 import Server from "../server";
 
 new Server().getMyApp().then((app: express.Application) => {
+  app.get("/", (req,res) => {
+    res.send("Hey!");
+  });
   app.listen(3000, () => {
     console.log("now listening on port 3000");
   });
