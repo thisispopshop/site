7949ab5f0557a5631d2040ed9e9b67330ae41bd9
kenakingkong <kenak@DESKTOP-T22M1KF.localdomain>
2019-03-22 20:23:20 -0700

    added path in client

--- client/src/utils/api.utils.ts
+++ client/src/utils/api.utils.ts
@@ -1,5 +1,5 @@
 export let APIConfig = {
-  url: "http://localhost:3000",
+  url: "http://ec2-13-52-175-75.us-west-1.compute.amazonaws.com/:3000",
   buildUrl(path: string): string {
     return `${this.url}${path}`;
   }
