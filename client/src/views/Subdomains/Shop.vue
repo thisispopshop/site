1a3362e9e8f948ab30311e983d39e7575c525002
kenakingkong <kenak@DESKTOP-T22M1KF.localdomain>
2019-04-15 14:16:15 -0700

    fixed shop page

--- client/src/views/Subdomains/Shop.vue
+++ client/src/views/Subdomains/Shop.vue
@@ -29,50 +29,24 @@ import {iProduct,iImage, iOccasion, iOrganization, iEvent} from "@/models";
 import ShopOccasion from "@/components/ShopOccasion.vue";
 
 @Component({
-  components: {
-    ShopOccasion
-  }
+    components: {
+      ShopOccasion
+    }
 })
 export default class Shop extends Vue {
-
-  //@Prop() org!: iOrganization;
-  //@Prop() event!: iEvent;
-  //@Prop() occasions! : iOccasion[];
+  @Prop() org!: iOrganization;
+  @Prop() event!: iEvent;
+  @Prop() occasions! : iOccasion[];
   //@Prop() selectedOccasion! : iOccasion;
-
-  public org! : iOrganization;
-  public event!: iEvent;
-  public occasions! : iOccasion[];
-  public error : string | boolean = false;
-  public selectedOccasion!: iOccasion;
   error : string | boolean = false;
-
   @Prop() oid!: string;
+  
   @Watch("oid")
   update(){
     this.selectedOccasion = this.occasions[this.oid];
   }
   selectedOccasion = this.occasions[this.oid];
-
-  mounted(){
-      console.log("starting navigation to subdomain: " + this.org_name);
-      this.error = false;
-      axios.get(APIConfig.buildUrl("/api/organization?subdomain=" + this.org_name))
-        .then((response:AxiosResponse) => {
-          this.org = response.data.organizations[0];
-          this.event = this.org.events[0];
-          this.occasions = this.event.occasions;
-          this.selectedOccasion = this.occasions[0];
-          console.log(this.org);
-          console.log(this.event);
-          console.log(this.occasions);
-        })
-        .catch((res:AxiosError) => {
-          console.log("couldn't find organization?");
-          this.error = res.response && res.response.data.reason;
-        })
-  }
-
+  mounted(){}
   chooseOccasion(occasion:iOccasion, ind:number) {
     //this.selectedOccasion = occasion;
     this.$router.push("/shop/"+ind);
@@ -81,7 +55,6 @@ export default class Shop extends Vue {
 </script>
 
 <style lang="scss">
-
 /*breadcrumb style*/
 .my-breadcrumb {
   float: right;
@@ -91,7 +64,6 @@ export default class Shop extends Vue {
 .crumb:hover {
   font-weight: bold;
 }
-
 /*page title style*/
 .page-title {
   padding-top: 3%;
@@ -99,6 +71,4 @@ export default class Shop extends Vue {
   margin-left: 20%;
   text-align: left;
 }
-
-
 </style>
